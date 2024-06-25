2f4862e0b2fac33236f58018fd69f732843d7414
ratnabharathi <100298110+ratnabharathi@users.noreply.github.com>
2024-06-25 15:13:21 +0530

    1

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,31 +1,24 @@
 import React from "react";
-import "./HeroSection.css";
-import headphoneImage from "../../Assets/headphone.png";
+import "./Hero.module.css";
+import headphoneImage from "../../assets/hero-image.png";
 
-/**
- * Represents the hero section of the application.
- * Displays a left section with text and a right section with an image.
- * @returns {JSX.Element} The rendered hero section component.
- */
-const HeroSection = () => {
+const Hero = () => {
     return ( <
         div className = "hero" >
         <
-        div className = "hero_left" >
+        div className = "hero-left" >
         <
         h3 > 100 Thousand Songs, ad - free < /h3> <
-        h3 > Over thousands podcast episodes < /h3> <
-        /div> <
-        div className = "hero_right" >
+        h3 > Over thousands of podcast episodes < /h3> <
+        /div>  <
+        div className = "hero-right" >
         <
         img src = { headphoneImage }
-        height = "250px"
-        width = "250px"
-        alt = "headphoneImage" / >
+        width = { 212 }
+        alt = "Headphones" / >
         <
-        /div> <
-        /div>
+        /div> </div >
     );
 };
 
-export default HeroSection;
+export default Hero;
