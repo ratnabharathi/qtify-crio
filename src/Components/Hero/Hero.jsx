bbe5b093bdffb4d5cc8c8971ac19c0807cd805c9
ratnabharathi <100298110+ratnabharathi@users.noreply.github.com>
2024-06-25 15:34:19 +0530

    1

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,23 +1,24 @@
 import React from "react";
-import "./Hero.module.css";
+import styles from "./Hero.module.css"; // Using CSS modules for better styling management
 import headphoneImage from "../../assets/hero-image.png";
 
 const Hero = () => {
     return ( <
-        div className = "hero" >
+        div className = { styles.hero } >
         <
-        div className = "hero-left" >
+        div className = { styles.textContainer } >
         <
         h3 > 100 Thousand Songs, ad - free < /h3> <
         h3 > Over thousands of podcast episodes < /h3> <
-        /div>  <
-        div className = "hero-right" >
+        /div> <
+        div className = { styles.imageContainer } >
         <
         img src = { headphoneImage }
         width = { 212 }
         alt = "Headphones" / >
         <
-        /div> </div >
+        /div> <
+        /div>
     );
 };
 
