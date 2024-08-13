f0f5801011dea4a59447f57aecfa7b9eab424a43
ratnabharathi <100298110+ratnabharathi@users.noreply.github.com>
2024-08-13 18:24:55 +0530

    3

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,18 +2,15 @@ import React from "react";
 import styles from "./Hero.module.css";
 
 function Hero() {
-    return ( < div className = { styles.hero } >
-        <
-        div >
-        <
-        h1 > 100 Thousand Songs, ad - free < /h1>  <
-        h1 > Over thousands podcast episodes < /h1>  <
-        /div> <div>  <
-        img src = { require("../../assets/hero-image.png") }
+    return ( <div className = { styles.hero } >
+        <div>
+  <h1> 100 Thousand Songs, ad-free </h1>  
+ <h1> Over thousands podcast episodes </h1>
+ </div> <div> 
+ <img src = { require("../../assets/hero-image.png") }
         width = { 212 }
-        alt = "headphones" / >
-        <
-        /div> </div >
+        alt = "headphones" />
+        </div> </div>
     );
 }
 export default Hero;
