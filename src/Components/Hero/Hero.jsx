ed20f839b7673ca20b856ceee97e81005d7b4675
ratnabharathi <100298110+ratnabharathi@users.noreply.github.com>
2024-08-13 17:43:15 +0530

    1

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,8 +7,8 @@ function Hero() {
         div >
         <
         h1 > 100 Thousand Songs, ad - free < /h1>  <
-        h1 > Over thousands podcast episodes < /h1>   <
-        /div> <div> <
+        h1 > Over thousands podcast episodes < /h1>  <
+        /div> <div>  <
         img src = { require("../../assets/hero-image.png") }
         width = { 212 }
         alt = "headphones" / >
